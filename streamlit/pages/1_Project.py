8f66b36c7d7e413019127037e2819817fd920d12
Aneifer <132136730+Aneifer@users.noreply.github.com>
2024-03-20 12:51:58 +0100

    updated project page streamlit

--- streamlit/pages/1_Project.py
+++ streamlit/pages/1_Project.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 import numpy as np
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.set_page_config(page_title="Project europarl.ai", page_icon="")
 
 st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
