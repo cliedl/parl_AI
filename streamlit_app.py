a798b875781b3b97900ea78f8f21acde74dcbc5a
cliedl <christian.liedl@gmx.net>
2024-03-20 13:38:46 +0100

    added english translation of app

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 from RAG_clean.models.generation import generate_chain_with_balanced_retrieval
 from RAG_clean.database.vector_database import VectorDatabase
+from app_utils.translate import translate
 
 # from RAG_clean.models.embedding import ManifestoBertaEmbeddings
 
@@ -71,6 +72,8 @@ if "feedback" not in st.session_state:
     st.session_state.feedback = None
 if "feedback_key" not in st.session_state:
     st.session_state.feedback_key = 0
+if "language" not in st.session_state:
+    st.session_state.language = "german"
 
 
 # def streaming(text, delay=DELAY):
@@ -128,18 +131,34 @@ def generate_response():
 
 ### INTERFACE ###
 st.title("🇪🇺 europarl.ai 2024")
-
+st.session_state.language = st.selectbox("Language", ["Deutsch", "English"])
+# st.radio(label="Language", ["Deutsch", "English"])
 query = st.text_input(
-    label="Gib ein Stichwort ein oder stelle eine Frage an die Parteien zur Europawahl:",
-    placeholder="Wie stehen die Parteien zum Emmissionshandel?",
+    label=translate(
+        "Gib ein Stichwort ein oder stelle eine Frage an die Parteien zur Europawahl:",
+        st.session_state.language,
+    ),
+    placeholder=translate(
+        "Wie stehen die Parteien zum Emmissionshandel?",
+        language=st.session_state.language,
+    ),
     value="",
 )
 
-st.button("Frage stellen", on_click=submit_query, type="primary")
+st.button(
+    translate("Frage stellen", st.session_state.language),
+    on_click=submit_query,
+    type="primary",
+)
 
 # STAGE 1: GENERATE RESPONSE
 if st.session_state.stage == 1:
-    with st.spinner("Suche nach Antworten in Wahlprogrammen... 🕵️‍♂️"):
+    with st.spinner(
+        translate(
+            "Suche nach Antworten in Wahlprogrammen...", st.session_state.language
+        )
+        + "🕵️"
+    ):
         generate_response()
 
     st.session_state.logged_prompt = collector.log_prompt(
@@ -152,7 +171,9 @@ if st.session_state.stage == 1:
 
 # STAGE >= 1: DISPLAY RESPONSE
 if st.session_state.stage > 1:
-    st.markdown(":grey[Die Reihenfolge der Parteien ist zufällig.]")
+    st.markdown(
+        f":grey[{translate('Die Reihenfolge der Parteien ist zufällig.', st.session_state.language,)}]",
+    )
 
     col1, col2 = st.columns([0.3, 0.7])
     col3, col4 = st.columns([0.3, 0.7])
@@ -194,16 +215,29 @@ if st.session_state.stage > 1:
             st.header(party_dict[party]["name"])
             st.write(st.session_state.response["answer"][party])
             st.write(
-                f'Mehr dazu im [Europawahlprogramm der Partei **{party_dict[party]["name"]}** (z.B. Seite {most_relevant_manifesto_page_number+1})]({party_dict[party]["manifesto_link"]}#page={most_relevant_manifesto_page_number+1})'
+                f"""{translate('Mehr dazu im', st.session_state.language)}
+                    [{translate('Europawahlprogramm der Partei', st.session_state.language)} 
+                    **{party_dict[party]['name']}** 
+                    ({translate('z.B. Seite', st.session_state.language)} {most_relevant_manifesto_page_number+1})]({party_dict[party]['manifesto_link']}#page={most_relevant_manifesto_page_number+1})"""
             )
         i += 2
 
-    st.subheader("Worauf basieren diese Antworten?")
-    st.write(
-        "Die Antworten wurden von dem KI-Sprachmodell GPT 3.5 generiert – unter Berücksichtigung der Wahlprogramme zur Europawahl 2024 und vergangenen Reden im Europaparlament im Zeitraum 2019-2024."
+    st.subheader(
+        translate("Worauf basieren diese Antworten?", st.session_state.language)
     )
-    st.write("Hier kannst du die genutzten Ausschnitte aus den Quellen einsehen:")
-    with st.expander("Quellen anzeigen"):
+    st.write(
+        translate(
+            "Die Antworten wurden von dem KI-Sprachmodell GPT 3.5 generiert – unter Berücksichtigung der Wahlprogramme zur Europawahl 2024 und vergangenen Reden im Europaparlament im Zeitraum 2019-2024.",
+            st.session_state.language,
+        )
+    )
+    st.write(
+        translate(
+            "Hier kannst du die genutzten Ausschnitte aus den Quellen einsehen:",
+            st.session_state.language,
+        )
+    )
+    with st.expander(translate("Quellen anzeigen", st.session_state.language)):
         for party in parties:
             st.subheader(party_dict[party]["name"])
             for doc in st.session_state.response["docs"]["manifestos"][party]:
@@ -216,9 +250,14 @@ if st.session_state.stage > 1:
             #     st.write(f"Rede: {doc.page_content}")
 
     st.markdown("---")
-    st.write("### Waren diese Antworten hilfreich für dich?")
     st.write(
-        "Mit deinem Feedback hilfst du uns, die Qualität der Antworten zu verbessern."
+        f"### {translate('Waren diese Antworten hilfreich für dich?', st.session_state.language)}"
+    )
+    st.write(
+        translate(
+            "Mit deinem Feedback hilfst du uns, die Qualität der Antworten zu verbessern.",
+            st.session_state.language,
+        )
     )
 
     st.session_state.stage = 3
@@ -238,4 +277,7 @@ if st.session_state.stage == 3:
     )
 
     if st.session_state.feedback is not None:
-        st.write("Vielen Dank für dein Feedback! 🙏")
+        st.write(
+            translate("Vielen Dank für dein Feedback!", st.session_state.language)
+            + " 🙏"
+        )
