57d01da25fbeaf605fc7ec11224afa3ae743f320
Aneifer <132136730+Aneifer@users.noreply.github.com>
2024-03-21 11:12:08 +0100

    changed FAQs

--- pages/1_FAQ.py
+++ pages/1_FAQ.py
@@ -12,33 +12,10 @@ st.subheader('Was ist europarl.ai?')
 
 st.write(
     """
-europarl.ai ist eine App, die dir hilft, die Positionen verschiedener Parteien zu Themen, die dir wichtig sind, herauszufinden. Perfekt für die bevorstehende Europawahl!
-
-    """
-)
+europarl.ai ist eine App, mit der du die Positionen der verschiedener Parteien komprimiert und übersichtlich zusammengefasst bekommst. 
 
 
-st.subheader('Warum wurde europarl.ai entwickelt?')
 
-st.write(
-    """
-Weil Europa spannend, aber auch ziemlich komplex ist. Wir wollen es einfacher machen, zu verstehen, wofür die verschiedenen Parteien stehen, besonders wenn es um wichtige Entscheidungen wie die Europawahl geht.
-
-    """
-)
-
-st.subheader('Wie kann europarl.ai mir bei der Wahl helfen?')
-st.write(
-    """
-Indem du einfach ein Thema auswählst oder eine Frage stellst, durchsucht europarl.ai die Wahlprogramme und Parlamentsreden, um dir eine klare Zusammenfassung der Parteipositionen zu geben.
-
-    """
-)
-
-st.subheader('Welche Themen deckt europarl.ai ab?')
-st.write(
-    """
-Alles von Energiepolitik über Renten bis hin zur Migrationspolitik – europarl.ai deckt eine breite Palette von Themen ab, die auf europäischer Ebene wichtig sind.
 
     """
 )
@@ -47,8 +24,18 @@ st.subheader('Woher kommen die Informationen in der App?')
 
 st.write(
     """
-Die Informationen werden aus den offiziellen Wahlprogrammen der deutschen Parteien für die Europawahl und den Reden dieser Parteien im EU-Parlament bezogen.
+Aus den Wahlprogrammen der deutschen Parteien zur Europawahl und den Reden von deutschen Politiker:innen im Europarlament 
 
+
+    """
+)
+
+st.subheader('Warum wurde europarl.ai entwickelt?')
+
+st.write(
+    """
+europarl.ai ist unser Abschluss-Projekt für das "Data Science Retreat", ein Bootcamp in Berlin bei dem wir Techniken rund um Data Scince, Maschinelles Lernen und Künstliche Intelligenz erlernt haben.
+Mit europarl.ai wollen wir es Wähler:innen erleichtern 
 
     """
 )
@@ -63,29 +50,19 @@ Nein, europarl.ai liefert dir eine Zusammenfassung der Positionen der größten 
     """
 )
 
-st.subheader('Kann europarl.ai mir auch helfen, wenn ich nicht viel über EU-Politik weiß?')
-st.write(
-    """
-Genau dafür ist es da! Die App ist benutzerfreundlich und bietet dir die Informationen, die du brauchst, um eine informierte Entscheidung zu treffen, unabhängig von deinem Vorwissen.
-
-    """
-)
-
-
-st.subheader('Was mache ich, wenn ich spezifische Fragen zu einem Thema habe?')
-
-st.write(
-    """
-Alles von Energiepolitik über Renten bis hin zur Migrationspolitik – europarl.ai deckt eine breite Palette von Themen ab, die auf europäischer Ebene wichtig sind.Gib einfach deine Frage in die App ein, und europarl.ai wird die relevanten Daten durchsuchen, um dir eine präzise Antwort zu geben.
-    """
-)
-
-
 st.subheader('Ist europarl.ai neutral und unparteiisch?')
 
 st.write(
     """
 Die App zielt darauf ab, objektive Informationen zu liefern, indem sie direkt aus den Quellen der Parteien schöpft, ohne eigene Meinungen oder Interpretationen hinzuzufügen.
+
+    """
+)
+
+st.subheader('Wie kann europarl.ai mir bei der Wahl helfen?')
+st.write(
+    """
+Indem du einfach ein Thema auswählst oder eine Frage stellst, durchsucht europarl.ai die Wahlprogramme und Parlamentsreden, um dir eine klare Zusammenfassung der Parteipositionen zu geben.
 
     """
 )
