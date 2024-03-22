fa70cfd5b85e9dda8e1c47db2e5ab0907764b21e
cliedl <christian.liedl@gmx.net>
2024-03-22 13:24:24 +0100

    Some changes in the FAQ and Team page

--- pages/1_FAQ.py
+++ pages/1_FAQ.py
@@ -2,61 +2,65 @@ import streamlit as st
 import time
 import numpy as np
 
-st.set_page_config(page_title="Projekt europarl.ai", page_icon=":iphone:")
+st.set_page_config(page_title="europarl.ai - FAQ", page_icon="🇪🇺")
 
 st.sidebar.header("Mehr zu europarl.ai")
 
-st.header('Am 9. Juni ist Europawahl. ✖️ :Informier Informier dich mit europarl.ai!', divider='blue')
+st.header(
+    "Am 9. Juni ist Europawahl. 🗳️ :Informier Informier dich mit europarl.ai!",
+    divider="blue",
+)
 
-st.subheader('Was ist europarl.ai?')
+st.subheader("Was ist europarl.ai?")
 
 st.write(
     """
-europarl.ai ist eine App, mit der du die Positionen der verschiedener Parteien komprimiert und übersichtlich zusammengefasst bekommst. 
-Im Hintergrund nutzen wir eine Retrieval Augmented Generation (RAG) Engine, das tausende von politischen Dokumenten durchsucht. 
-Die Stellen in den Dokumenten, die am ähnlichsten zu den keywords in deiner Frage sind, werden rausgepickt und mithilfe eines Large Language Models präzise zusammengefasst.
+europarl.ai ist eine App, mit der du dich zur Europawahl 2024 informieren kannst. 
+Stelle deine Frage oder gib ein Stichwort ein und unser Modell fasst die Positionen der Parteien zu deinem Input zusammen.
+
+Im Hintergrund nutzen wir eine Retrieval Augmented Generation (RAG) Engine, die Zugriff auf tausende politische Dokumenten hat. 
+Unser Algorithmus sucht nach den Dokumente die am relevantesten für deinen Input sind. 
+Mit Hilfe dieser Information generiert ein KI Sprachmodell dann übersichtliche Zusammenfassungen für die verschiedenen Parteien.
+
 So erhältst du in der europarl.ai - App eine übersichtliche Zusammenfassung.
-
     """
 )
 
 
-st.subheader('Wie stellt ihr sicher, dass das LLM nicht halluziniert?')
+st.subheader("Wie stellt ihr sicher, dass das LLM nicht halluziniert?")
 
 st.write(
     """
-Wir nutzen sehr präzise Prompts und konservative Einstellungen für das LLMs. Wenn deine Frage anhand der politischen Dokumente nicht beantworten kann, antwortet es, dass es keine Informationen dazu gefunden hat.
+Wir nutzen sehr präzise Prompts und konservative Einstellungen für das LLMs. Wenn deine Frage anhand der politischen Dokumente nicht beantwortet werden kann, sollte der Algorithmus antworten, dass es keine Informationen dazu gefunden hat.
 Außerdem legen wir die Quellen offen, auf denen die Antworten in der App basieren. So kannst du selbst überprüfen, wie deine Antwort zustande gekommen ist.
-Dennoch lassen sich Bugs zu 100 Prozent kaum ausschließen. Über einen Feedbackbutton in der App kannst du uns auf Fehler oder Ungereimtheiten in der Antwort aufmerksam machen.
-
-
+Dennoch lassen sich Fehler zu 100 Prozent kaum ausschließen. Über einen Feedbackbutton in der App kannst du uns auf Fehler oder Ungereimtheiten in der Antwort aufmerksam machen.
     """
 )
 
 
-st.subheader('Welche Daten verwendet ihr für die App?')
+st.subheader("Welche Daten verwendet ihr für die App?")
 
 st.write(
     """
-Wir nutzen die öffentlichen Wahlprogramme der deutschen Parteien zur Europawahl. Außerdem laden wir die öffentlichen Reden von deutschen Politiker_innen im Europarlament aus der Legislaturperiode 2019 - 2024 über eine API.
-
-
+Wir nutzen die öffentlichen Wahlprogramme der deutschen Parteien zur Europawahl. 
+Dabei haben wir uns zunächst auf die 6 größten Partein, die auch im Bundestag vertreten sind, beschränkg.
+Außerdem nutzen wir öffentlichen Reden von deutschen Politiker_innen im Europarlament aus der Legislaturperiode 2019 - 2024.
     """
 )
 
 
-st.subheader('Bekomme ich Informationen zu jeder Partei?')
+st.subheader("Bekomme ich Informationen zu jeder Partei?")
 
 st.write(
     """
 Nein, europarl.ai liefert dir eine Zusammenfassung der Positionen der größten Parteien in Deutschland. Es treten jedoch noch sehr viel mehr Kleinstparteien bei der Europawahl an, da es im Gegensatz zur Bundeswahl keine pozentuale Sperrklausel gibt für den Einzug ins Parlament.
-Einen Überblick über alle weiteren Parteien findest du hier.
+Einen Überblick über alle weiteren Parteien findest du [hier](https://www.europawahl-bw.de/deutsche-parteien).
 
 
     """
 )
 
-st.subheader('Ist europarl.ai neutral und unparteiisch?')
+st.subheader("Ist europarl.ai neutral und unparteiisch?")
 
 st.write(
     """
@@ -65,7 +69,7 @@ Hinter dem Projekt stehen keine Parteien oder Unternehmen.
 
     """
 )
-st.subheader('Wie aktuell ist europarl.ai?')
+st.subheader("Wie aktuell ist europarl.ai?")
 
 st.write(
     """
@@ -74,12 +78,12 @@ Die App ist Anfang April 2024 fertiggestellt worden und wird Anfang Juni 2024 vo
     """
 )
 
-st.subheader('Warum wurde europarl.ai entwickelt?')
+st.subheader("Warum wurde europarl.ai entwickelt?")
 
 st.write(
     """
 europarl.ai ist unser Abschluss-Projekt für das "Data Science Retreat", ein Bootcamp in Berlin bei dem wir Techniken rund um Data Scince, Maschinelles Lernen und Künstliche Intelligenz erlernt haben.
-Mit europarl.ai wollen wir es Wähler_innen erleichtern, sich vor der Europawahl 2024 zu informieren.
+Mit europarl.ai wollen wir es Wähler_innen erleichtern, sich vor der Europawahl 2024 besser über die Standpunkte der verschiedenen Parteien zu informieren.
 
     """
 )
