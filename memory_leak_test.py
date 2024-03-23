bff1740ccbde9358e0eb110b234c619a20a70189
cliedl <christian.liedl@gmx.net>
2024-03-23 17:46:58 +0100

    added script for debugging memory leak problem

---
+++ memory_leak_test.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import random
+from trubrics.integrations.streamlit import FeedbackCollector
+import os
+import re
+import csv
+import random
+from datetime import datetime
+import time
+import psutil
+
+process = psutil.Process()
+
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+from RAG.models.generation import generate_chain
+from RAG.database.vector_database import VectorDatabase
+from streamlit_app.utils.translate import translate
+from RAG.models.generation import build_context_for_party
+
+
+def run():
+    DATABASE_DIR_MANIFESTOS = "./data/manifestos/chroma/openai"
+    DATABASE_DIR_DEBATES = "./data/debates/chroma/openai"
+    DELAY = 0.05  # pause between words in text stream (in seconds)
+    TEMPERATURE = 0.0
+    LARGE_LANGUAGE_MODEL = ChatOpenAI(
+        model_name="gpt-3.5-turbo", max_tokens=400, temperature=TEMPERATURE
+    )
+
+    @st.cache_resource
+    def load_embedding_model():
+        return OpenAIEmbeddings(model="text-embedding-3-large")
+
+    embedding_model = load_embedding_model()
+
+    @st.cache_resource
+    def load_db_manifestos():
+        return VectorDatabase(
+            embedding_model=embedding_model,
+            source_type="manifestos",
+            database_directory=DATABASE_DIR_MANIFESTOS,
+        )
+
+    db_manifestos = load_db_manifestos()
+
+    @st.cache_resource
+    def load_db_debates():
+        return VectorDatabase(
+            embedding_model=embedding_model,
+            source_type="debates",
+            database_directory=DATABASE_DIR_DEBATES,
+        )
+
+    db_debates = load_db_debates()
+
+    chain = generate_chain(
+        [db_manifestos],
+        llm=LARGE_LANGUAGE_MODEL,
+        language="Deutsch",
+        k=3,
+    )
+
+    query = "Wie stehen die Parteien zum Krieg in der Ukraine?"
+    context = {}
+    for party in ["afd", "cdu", "fdp", "spd", "gruene", "linke"]:
+        # context = build_context_for_party(db_manifestos, query=query, party=party, k=5)
+        docs = db_manifestos.database.max_marginal_relevance_search(
+            query, k=5, fetch_k=20, filter={"party": party}
+        )
+        context[party] = build_context_for_party(
+            db_manifestos,
+            party=party,
+            query=query,
+            k=3,
+        )
+    language = "Deutsch"
+
+    prompt = f"""
+    Beantworte die unten folgende FRAGE DES NUTZERS, indem du die politischen Positionen der Partei im unten angegebenen KONTEXT zusammenfasst.
+    Der KONTEXT umfasst Ausschnitte aus Redebeiträgen im EU-Parlament und aus dem EU-Wahlprogramm für 2024 für die Partei.
+    Deine Antwort soll ausschließlich die Informationen aus dem genannten KONTEXT beinhalten.
+    Verwende in deiner Antwort NICHT den Namen der Partei, sondern beziehe dich auf die Partei ausschließlich mit "die Partei".
+    Sollte der KONTEXT keine Antwort auf die FRAGE DES NUTZERS zulassen, gib anstelle der Zusammenfassung NUR die folgende Rückmeldung:
+    "Es wurde keine passende Antwort in den Quellen gefunden."
+    Gib die Antwort auf {language}.
+
+    KONTEXT:
+    {context["gruene"]}
+
+    FRAGE DES NUTZERS:
+    {query}
+    """
+    response = LARGE_LANGUAGE_MODEL.invoke(prompt)
+    print(f"Memory usage {process.memory_info().rss/(1024**2)} MB")
+
+
+while True:
+    run()
+    time.sleep(1)
