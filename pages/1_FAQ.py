c6b17923809c0a5cab4a5e781ccf77e851b64435
Aneifer <132136730+Aneifer@users.noreply.github.com>
2024-03-23 21:11:22 +0100

    Updated 1_FAQ.py added github account corrected mistakes

--- pages/1_FAQ.py
+++ pages/1_FAQ.py
@@ -20,7 +20,7 @@ Stelle deine Frage oder gib ein Stichwort ein und unser Modell fasst die Positio
 
 Im Hintergrund nutzen wir eine Retrieval Augmented Generation (RAG) Engine, die Zugriff auf tausende politische Dokumenten hat. 
 Unser Algorithmus sucht nach den Dokumente die am relevantesten für deinen Input sind. 
-Mit Hilfe dieser Information generiert ein KI Sprachmodell dann übersichtliche Zusammenfassungen für die verschiedenen Parteien.
+Mit Hilfe dieser Information generiert ein KI-Sprachmodell dann übersichtliche Zusammenfassungen für die verschiedenen Parteien.
 
 So erhältst du in der europarl.ai - App eine übersichtliche Zusammenfassung.
     """
@@ -31,9 +31,9 @@ st.subheader("Wie stellt ihr sicher, dass das LLM nicht halluziniert?")
 
 st.write(
     """
-Wir nutzen sehr präzise Prompts und konservative Einstellungen für das LLMs. Wenn deine Frage anhand der politischen Dokumente nicht beantwortet werden kann, sollte der Algorithmus antworten, dass es keine Informationen dazu gefunden hat.
+Wir nutzen sehr präzise Prompts und konservative Einstellungen für das LLM. Wenn deine Frage anhand der politischen Dokumente nicht beantwortet werden kann, sollte der Algorithmus antworten, dass es keine Informationen dazu gefunden hat.
 Außerdem legen wir die Quellen offen, auf denen die Antworten in der App basieren. So kannst du selbst überprüfen, wie deine Antwort zustande gekommen ist.
-Dennoch lassen sich Fehler zu 100 Prozent kaum ausschließen. Über einen Feedbackbutton in der App kannst du uns auf Fehler oder Ungereimtheiten in der Antwort aufmerksam machen.
+Dennoch lassen sich Fehler zu 100 Prozent kaum ausschließen. Über einen Feedbackbutton in der App kannst du uns auf Fehler oder Ungereimtheiten in der Antwort aufmerksam machen. Dadurch kannst du uns helfen, europarl.ai zu verbessern.
     """
 )
 
@@ -43,17 +43,24 @@ st.subheader("Welche Daten verwendet ihr für die App?")
 st.write(
     """
 Wir nutzen die öffentlichen Wahlprogramme der deutschen Parteien zur Europawahl. 
-Dabei haben wir uns zunächst auf die 6 größten Partein, die auch im Bundestag vertreten sind, beschränkg.
-Außerdem nutzen wir öffentlichen Reden von deutschen Politiker_innen im Europarlament aus der Legislaturperiode 2019 - 2024.
+In der App findest du nur sechs größten deutschen Partein, die auch im Bundestag vertreten sind.
+Außerdem nutzen wir die öffentlichen Reden von deutschen Politiker_innen im Europarlament aus der Legislaturperiode der Jahre 2019 - 2024.
     """
 )
 
+st.subheader("Wo finde ich den Code von europarl.ai?")
+
+st.write(
+    """
+Das Projekt europarl.ai findest du auf [GitHub](https://github.com/europarl-ai/europarl-ai). Das Projekt ist Open-Source und mit einer MIT-Lizenz lizensiert.
+    """
+)
 
 st.subheader("Bekomme ich Informationen zu jeder Partei?")
 
 st.write(
     """
-Nein, europarl.ai liefert dir eine Zusammenfassung der Positionen der größten Parteien in Deutschland. Es treten jedoch noch sehr viel mehr Kleinstparteien bei der Europawahl an, da es im Gegensatz zur Bundeswahl keine pozentuale Sperrklausel gibt für den Einzug ins Parlament.
+Nein, europarl.ai liefert dir eine Zusammenfassung der Positionen der größten Parteien in Deutschland. Es treten jedoch noch sehr viel mehr Kleinstparteien bei der Europawahl an, da es im Gegensatz zur Wahl des Bundestages keine pozentuale Sperrklausel gibt für den Einzug ins EU-Parlament.
 Einen Überblick über alle weiteren Parteien findest du [hier](https://www.europawahl-bw.de/deutsche-parteien).
 
 
@@ -64,7 +71,7 @@ st.subheader("Ist europarl.ai neutral und unparteiisch?")
 
 st.write(
     """
-Die App zielt darauf ab, objektive Informationen zu liefern, indem sie direkt aus den Quellen der Parteien schöpft, ohne die persönlichen Meinungen oder Interpretationen der Macher hinzuzufügen.
+Die App zielt darauf ab, objektive Informationen zu liefern, indem sie direkt aus den Quellen der Parteien und Reden der Politiker_innen schöpft. Dabei werden weder die persönlichen Meinungen noch Interpretationen der Macher_innen hinzugefügt.
 Hinter dem Projekt stehen keine Parteien oder Unternehmen.
 
     """
@@ -82,7 +89,7 @@ st.subheader("Warum wurde europarl.ai entwickelt?")
 
 st.write(
     """
-europarl.ai ist unser Abschluss-Projekt für das "Data Science Retreat", ein Bootcamp in Berlin bei dem wir Techniken rund um Data Scince, Maschinelles Lernen und Künstliche Intelligenz erlernt haben.
+europarl.ai ist unser Abschluss-Projekt für das ["Data Science Retreat"](https://datascienceretreat.com/), ein Bootcamp in Berlin bei dem wir Techniken rund um Data Scince, Maschinelles Lernen und Künstliche Intelligenz erlernt haben.
 Mit europarl.ai wollen wir es Wähler_innen erleichtern, sich vor der Europawahl 2024 besser über die Standpunkte der verschiedenen Parteien zu informieren.
 
     """
