050695162293e60eb4b52d6b83091f572ed515a2
cliedl <christian.liedl@gmx.net>
2024-03-23 18:29:21 +0100

    Added memory leak test: Seems to be langchain invoke

--- memory_leak_test.py
+++ memory_leak_test.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import time
 import psutil
 
+
 process = psutil.Process()
 
 
@@ -19,6 +20,8 @@ from RAG.database.vector_database import VectorDatabase
 from streamlit_app.utils.translate import translate
 from RAG.models.generation import build_context_for_party
 
+print(f"Memory usage {process.memory_info().rss/(1024**2)} MB")
+
 
 def run():
     DATABASE_DIR_MANIFESTOS = "./data/manifestos/chroma/openai"
@@ -28,6 +31,7 @@ def run():
     LARGE_LANGUAGE_MODEL = ChatOpenAI(
         model_name="gpt-3.5-turbo", max_tokens=400, temperature=TEMPERATURE
     )
+    USE_LANGCHAIN = False
 
     @st.cache_resource
     def load_embedding_model():
@@ -56,46 +60,59 @@ def run():
     db_debates = load_db_debates()
 
     chain = generate_chain(
-        [db_manifestos],
+        [db_manifestos, db_debates],
         llm=LARGE_LANGUAGE_MODEL,
         language="Deutsch",
         k=3,
     )
 
+    t0 = time.time()
     query = "Wie stehen die Parteien zum Krieg in der Ukraine?"
-    context = {}
-    for party in ["afd", "cdu", "fdp", "spd", "gruene", "linke"]:
-        # context = build_context_for_party(db_manifestos, query=query, party=party, k=5)
-        docs = db_manifestos.database.max_marginal_relevance_search(
-            query, k=5, fetch_k=20, filter={"party": party}
-        )
-        context[party] = build_context_for_party(
-            db_manifestos,
-            party=party,
-            query=query,
-            k=3,
-        )
-    language = "Deutsch"
 
-    prompt = f"""
-    Beantworte die unten folgende FRAGE DES NUTZERS, indem du die politischen Positionen der Partei im unten angegebenen KONTEXT zusammenfasst.
-    Der KONTEXT umfasst Ausschnitte aus Redebeiträgen im EU-Parlament und aus dem EU-Wahlprogramm für 2024 für die Partei.
-    Deine Antwort soll ausschließlich die Informationen aus dem genannten KONTEXT beinhalten.
-    Verwende in deiner Antwort NICHT den Namen der Partei, sondern beziehe dich auf die Partei ausschließlich mit "die Partei".
-    Sollte der KONTEXT keine Antwort auf die FRAGE DES NUTZERS zulassen, gib anstelle der Zusammenfassung NUR die folgende Rückmeldung:
-    "Es wurde keine passende Antwort in den Quellen gefunden."
-    Gib die Antwort auf {language}.
+    if USE_LANGCHAIN:
+        response = chain.invoke(query)
+    else:
+        parties = ["afd", "cdu", "fdp", "spd", "gruene", "linke"]
+        context = {}
+        response = {}
+        for party in parties:
+            # context = build_context_for_party(db_manifestos, query=query, party=party, k=5)
+            docs = db_manifestos.database.max_marginal_relevance_search(
+                query, k=5, fetch_k=20, filter={"party": party}
+            )
+            context[party] = build_context_for_party(
+                db_manifestos,
+                party=party,
+                query=query,
+                k=3,
+            )
+            language = "Deutsch"
 
-    KONTEXT:
-    {context["gruene"]}
+            prompt = f"""
+            Beantworte die unten folgende FRAGE DES NUTZERS, indem du die politischen Positionen der Partei im unten angegebenen KONTEXT zusammenfasst.
+            Der KONTEXT umfasst Ausschnitte aus Redebeiträgen im EU-Parlament und aus dem EU-Wahlprogramm für 2024 für die Partei.
+            Deine Antwort soll ausschließlich die Informationen aus dem genannten KONTEXT beinhalten.
+            Verwende in deiner Antwort NICHT den Namen der Partei, sondern beziehe dich auf die Partei ausschließlich mit "die Partei".
+            Sollte der KONTEXT keine Antwort auf die FRAGE DES NUTZERS zulassen, gib anstelle der Zusammenfassung NUR die folgende Rückmeldung:
+            "Es wurde keine passende Antwort in den Quellen gefunden."
+            Gib die Antwort auf {language}.
 
-    FRAGE DES NUTZERS:
-    {query}
-    """
-    response = LARGE_LANGUAGE_MODEL.invoke(prompt)
+            KONTEXT:
+            {context[party]}
+
+            FRAGE DES NUTZERS:
+            {query}
+            """
+
+            response[party] = LARGE_LANGUAGE_MODEL.invoke(prompt)
+
+    t1 = time.time()
     print(f"Memory usage {process.memory_info().rss/(1024**2)} MB")
 
+    return response, t1 - t0
 
-while True:
-    run()
+
+for i in range(10):
+    response, execution_time = run()
+    print(f"this took {execution_time} s")
     time.sleep(1)
