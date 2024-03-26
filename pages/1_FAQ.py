610f0fc533fd17ebda4044e1ce3b1e56815c62b4
Aneifer <132136730+Aneifer@users.noreply.github.com>
2024-03-26 11:03:11 +0100

    corrected spelling mistakes in FAQ

--- pages/1_FAQ.py
+++ pages/1_FAQ.py
@@ -18,8 +18,8 @@ st.write(
 europarl.ai ist eine App, mit der du dich zur Europawahl 2024 informieren kannst. 
 Stelle deine Frage oder gib ein Stichwort ein und unser Modell fasst die Positionen der Parteien zu deinem Input zusammen.
 
-Im Hintergrund nutzen wir eine Retrieval Augmented Generation (RAG) Engine, die Zugriff auf tausende politische Dokumenten hat. 
-Unser Algorithmus sucht nach den Dokumente die am relevantesten für deinen Input sind. 
+Im Hintergrund nutzen wir eine Retrieval Augmented Generation (RAG) Engine, die Zugriff auf tausende politische Dokumente hat. 
+Unser Algorithmus sucht nach den Dokumenten die am relevantesten für deinen Input sind. 
 Mit Hilfe dieser Information generiert ein KI-Sprachmodell dann übersichtliche Zusammenfassungen für die verschiedenen Parteien.
 
 So erhältst du in der europarl.ai - App eine übersichtliche Zusammenfassung.
@@ -43,7 +43,7 @@ st.subheader("Welche Daten verwendet ihr für die App?")
 st.write(
     """
 Wir nutzen die öffentlichen Wahlprogramme der deutschen Parteien zur Europawahl. 
-In der App findest du nur sechs größten deutschen Partein, die auch im Bundestag vertreten sind.
+In der App findest du nur die sechs größten deutschen Partein, die auch im EU-Parlament vertreten sind.
 Außerdem nutzen wir die öffentlichen Reden von deutschen Politiker_innen im Europarlament aus der Legislaturperiode der Jahre 2019 - 2024.
     """
 )
@@ -60,7 +60,7 @@ st.subheader("Bekomme ich Informationen zu jeder Partei?")
 
 st.write(
     """
-Nein, europarl.ai liefert dir eine Zusammenfassung der Positionen der größten Parteien in Deutschland. Es treten jedoch noch sehr viel mehr Kleinstparteien bei der Europawahl an, da es im Gegensatz zur Wahl des Bundestages keine pozentuale Sperrklausel gibt für den Einzug ins EU-Parlament.
+Nein, europarl.ai liefert dir eine Zusammenfassung der Positionen der größten Parteien in Deutschland. Es treten jedoch noch sehr viel mehr Kleinstparteien bei der Europawahl an, da es im Gegensatz zur Wahl des Bundestages keine prozentuale Sperrklausel gibt für den Einzug ins EU-Parlament.
 Einen Überblick über alle weiteren Parteien findest du [hier](https://www.europawahl-bw.de/deutsche-parteien).
 
 
@@ -89,7 +89,7 @@ st.subheader("Warum wurde europarl.ai entwickelt?")
 
 st.write(
     """
-europarl.ai ist unser Abschluss-Projekt für das ["Data Science Retreat"](https://datascienceretreat.com/), ein Bootcamp in Berlin bei dem wir Techniken rund um Data Scince, Maschinelles Lernen und Künstliche Intelligenz erlernt haben.
+europarl.ai ist unser Abschluss-Projekt für das ["Data Science Retreat"](https://datascienceretreat.com/), ein Bootcamp in Berlin bei dem wir Techniken rund um Data Science, Maschinelles Lernen und Künstliche Intelligenz erlernt haben.
 Mit europarl.ai wollen wir es Wähler_innen erleichtern, sich vor der Europawahl 2024 besser über die Standpunkte der verschiedenen Parteien zu informieren.
 
     """
