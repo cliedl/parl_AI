957a586424751d480f3136daf6771db86448f80e
Aneifer <132136730+Aneifer@users.noreply.github.com>
2024-03-26 10:53:38 +0100

    added impressum page

---
+++ pages/3_Impressum.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+from urllib.error import URLError
+
+st.set_page_config(page_title="europarl.ai - Team", page_icon="🇪🇺")
+
+
+st.header("Impressum und Datenschutzerklärung:", divider="blue")
+# st.sidebar.header("Daten")
+
+st.subheader("Impressum")
+st.write(
+    """
+    Folgt in Kürze.
+    """
+)
+
