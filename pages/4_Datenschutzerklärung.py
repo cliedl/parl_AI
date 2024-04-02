68d1828a307eb03fc0cc25d567ff6b4ec2dddfc6
cliedl <christian.liedl@gmx.net>
2024-04-02 18:03:36 +0200

    Updated Datenschutzerklärung (now with google cloud, OpenAI and buy me a coffee paragraph)

--- pages/4_Datenschutzerklärung.py
+++ pages/4_Datenschutzerklärung.py
@@ -13,7 +13,7 @@ st.header("Datenschutzerklärung", divider="blue")
 st.components.v1.html(
     """
 <p>Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der EU-Datenschutzgrundverordnung (DSGVO), ist:</p>
-<p>electify GbR, siehe Impressum</p>
+<p>Electify GbR, siehe Impressum</p>
 <h2>Ihre Betroffenenrechte</h2>
 <p>Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten können Sie jederzeit folgende Rechte ausüben:</p>
 <ul>
@@ -40,20 +40,37 @@ st.components.v1.html(
 <p>Wir verwenden Ihre Daten nicht, um Rückschlüsse auf Ihre Person zu ziehen. Informationen dieser Art werden von uns ggfs. anonymisiert statistisch ausgewertet, um unseren Internetauftritt und die dahinterstehende Technik zu optimieren. </p>
 <h3>Rechtsgrundlage und berechtigtes Interesse:</h3>
 <p>Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. f DSGVO auf Basis unseres berechtigten Interesses an der Verbesserung der Stabilität und Funktionalität unserer Website.</p>
-<h3>Empfänger:</h3>
-<p>Empfänger der Daten sind ggf. technische Dienstleister, die für den Betrieb und die Wartung unserer Webseite als Auftragsverarbeiter tätig werden.</p>
-<p></p><h3>Drittlandtransfer:</h3>
-<p>Die erhobenen Daten werden ggfs. in folgende Drittländer übertragen: </p>
-<p>USA</p>
-<p>Folgende Datenschutzgarantien liegen vor: </p>
-<p>Standard-Vertragsklauseln</p>
+
+<p></p><h3>Drittanbieter:</h3>
+<p>Wir nutzen den Webhoster Google Cloud der Google Ireland Ltd. (Irland, EU), die gemäß Artikel 28 DSGVO beauftragt wurde.
+Näheres zur Art und Weise der Verarbeitung bei diesem Drittanbieter ist hier beschrieben: <a href=https://cloud.google.com/run?hl=de>https://cloud.google.com/run?hl=de</a>. 
+Der Serverstandort befindet sich dabei in Belgien, EU.
+Es ist nicht auszuschließen, dass eine Datenübermittlung an die Muttergesellschaft Google LLC (USA) z.B. zu Wartungszwecken erfolgt.
+Dies steht der Beauftragung nicht entgegen, da sich die Google LLC (USA) gemäß den EU-Standardvertragsklauseln verpflichtet hat.</p>
+
+<p>Wir nutzen im Zusammenhang mit dem Einsatz künstlicher Intelligenz (KI) das API-Tool
+der OpenAI, LLC (USA), die gemäß Artikel 28 DSGVO beauftragt wurde. 
+Dabei werden Eingabedaten über ein Eingabefeld erhoben und an die externe Anwendung der OpenAI LLC übermittelt, die mit Hilfe künstlicher Intelligenz eine Antwort generiert.
+Aus dieser Interaktion werden Erkenntnisse gewonnen, die einerseits das KI-System trainieren und andererseits sowohl den
+Verantwortlichen als auch OpenAI LLC zur Verfügung stehen. Zweck ist die zielgerichtete
+und stets verfügbare Kommunikation mit den Betroffenen. Darin liegt auch das berechtigte Interesse.
+Hierbei werden folgende Daten verarbeitet: Eingabedaten, Erkenntnisse aus den Eingaben.
+Näheres zur Art und Weise der Verarbeitung bei diesem Drittanbieter ist hier beschrieben: <a href=https://openai.com/policies/privacy-policy>https://openai.com/policies/privacy-policy</a>. 
+Dem Einsatz dieses Drittanbieters steht nicht entgegen, dass dieser seinen Sitz außerhalb der EU
+hat, da der Anbieter hat sich gemäß den EU-Standardvertragsklauseln verpflichtet.</p>
+
+<p>Der Nutzer hat die Möglichkeit, seine Wertschätzung für die zur Verfügung gestellten Informationen durch eine Spende auszudrücken.  
+Dafür nutzen wir den Dienst „Buy me a Coffee“, der Publisherr Inc. (USA). Dabei werden Daten von „Buy me a Coffee“ verarbeitet und auf Systemen von Publisherr Inc. gespeichert. 
+Wir weisen darauf hin, dass wir keine Kenntnis vom Inhalt der übermittelten Daten sowie deren Nutzung durch „Buy me a Coffee“ erhalten. 
+Weitere Informationen hierzu finden Sie hier: <a href=https://www.buymeacoffee.com/privacy-policy> https://www.buymeacoffee.com/privacy-policy </a>.</p>
+
 <p></p><h3>Speicherdauer:</h3>
 <p>Die Daten werden gelöscht, sobald diese für den Zweck der Erhebung nicht mehr erforderlich sind. Dies ist für die Daten, die der Bereitstellung der Website dienen, grundsätzlich der Fall, wenn die jeweilige Sitzung beendet ist. </p>
 <p></p><h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
 <p>Die Bereitstellung der vorgenannten personenbezogenen Daten ist weder gesetzlich noch vertraglich vorgeschrieben. Ohne die IP-Adresse ist jedoch der Dienst und die Funktionsfähigkeit unserer Website nicht gewährleistet. Zudem können einzelne Dienste und Services nicht verfügbar oder eingeschränkt sein. Aus diesem Grund ist ein Widerspruch ausgeschlossen. </p>
 <p></p><h2>Cookies</h2>
 <p>Wie viele andere Webseiten verwenden wir auch so genannte „Cookies“. Bei Cookies handelt es sich um kleine Textdateien, die auf Ihrem Endgerät (Laptop, Tablet, Smartphone o.ä.) gespeichert werden, wenn Sie unsere Webseite besuchen. </p>
-<p>Sie können Sie einzelne Cookies oder den gesamten Cookie-Bestand löschen. Darüber hinaus erhalten Sie Informationen und Anleitungen, wie diese Cookies gelöscht oder deren Speicherung vorab blockiert werden können. Je nach Anbieter Ihres Browsers finden Sie die notwendigen Informationen unter den nachfolgenden Links:</p>
+<p>Sie können einzelne Cookies oder den gesamten Cookie-Bestand löschen. Darüber hinaus erhalten Sie Informationen und Anleitungen, wie diese Cookies gelöscht oder deren Speicherung vorab blockiert werden können. Je nach Anbieter Ihres Browsers finden Sie die notwendigen Informationen unter den nachfolgenden Links:</p>
 <ul>
 <li>Mozilla Firefox: <a href="https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen" target="_blank" rel="nofollow noopener">https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen</a></li>
 <li>Internet Explorer: <a href="https://support.microsoft.com/de-de/help/17442/windows-internet-explorer-delete-manage-cookies" target="_blank" rel="nofollow noopener">https://support.microsoft.com/de-de/help/17442/windows-internet-explorer-delete-manage-cookies</a></li>
@@ -63,21 +80,10 @@ st.components.v1.html(
 </ul>
 <h3>Speicherdauer und eingesetzte Cookies:</h3>
 <p>Soweit Sie uns durch Ihre Browsereinstellungen oder Zustimmung die Verwendung von Cookies erlauben, können folgende Cookies auf unseren Webseiten zum Einsatz kommen:</p>
-<p></p><h2>Technisch notwendige Cookies </h2>
-<h3>Art und Zweck der Verarbeitung: </h3>
 <p>Wir setzen Cookies ein, um unsere Website nutzerfreundlicher zu gestalten. Einige Elemente unserer Internetseite erfordern es, dass der aufrufende Browser auch nach einem Seitenwechsel identifiziert werden kann.</p>
 <p>Der Zweck der Verwendung technisch notwendiger Cookies ist, die Nutzung von Websites für die Nutzer zu vereinfachen. Einige Funktionen unserer Internetseite können ohne den Einsatz von Cookies nicht angeboten werden. Für diese ist es erforderlich, dass der Browser auch nach einem Seitenwechsel wiedererkannt wird.</p>
-<p>Für folgende Anwendungen benötigen wir Cookies:</p>
-<p>Übernahme von Spracheinstellungen</p>
 <h3>Rechtsgrundlage und berechtigtes Interesse: </h3>
 <p>Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. f DSGVO auf Basis unseres berechtigten Interesses an einer nutzerfreundlichen Gestaltung unserer Website.</p>
-<h3>Empfänger: </h3>
-<p>Empfänger der Daten sind ggf. technische Dienstleister, die für den Betrieb und die Wartung unserer Website als Auftragsverarbeiter tätig werden.</p>
-<p></p><h3>Drittlandtransfer:</h3>
-<p>Die erhobenen Daten werden ggfs. in folgende Drittländer übertragen: </p>
-<p>USA</p>
-<p>Folgende Datenschutzgarantien liegen vor: </p>
-<p>Standard-Vertragsklauseln</p>
 <p></p><h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
 <p>Die Bereitstellung der vorgenannten personenbezogenen Daten ist weder gesetzlich noch vertraglich vorgeschrieben. Ohne diese Daten ist jedoch der Dienst und die Funktionsfähigkeit unserer Website nicht gewährleistet. Zudem können einzelne Dienste und Services nicht verfügbar oder eingeschränkt sein.</p>
 <h3>Widerspruch</h3>
@@ -90,7 +96,7 @@ st.components.v1.html(
 <p>Sie haben das Recht, aus Gründen, die sich aus Ihrer besonderen Situation ergeben, jederzeit gegen die Verarbeitung Sie betreffender personenbezogener Daten, die aufgrund Art. 6 Abs. 1 lit. f DSGVO (Datenverarbeitung auf der Grundlage einer Interessenabwägung) erfolgt, Widerspruch einzulegen; dies gilt auch für ein auf diese Bestimmung gestütztes Profiling im Sinne von Art. 4 Nr. 4 DSGVO.</p>
 <p>Legen Sie Widerspruch ein, werden wir Ihre personenbezogenen Daten nicht mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten überwiegen, oder die Verarbeitung dient der Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen.</p>
 <h3>Empfänger eines Widerspruchs</h3>
-<p>electify GbR</p>
+<p>Electify GbR, siehe Impressum</p>
 <hr>
 <h2>Änderung unserer Datenschutzbestimmungen</h2>
 <p>Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie stets den aktuellen rechtlichen Anforderungen entspricht oder um Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen, z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt dann die neue Datenschutzerklärung.</p>
