059675724c0238e397bbb016a4b9ecce66431699
josh-nowak <joshuanowak@icloud.com>
2024-04-02 15:03:16 +0200

    added comments and docstrings in RAG.py

--- RAG/models/RAG.py
+++ RAG/models/RAG.py
@@ -3,6 +3,16 @@ import asyncio
 
 
 class RAG:
+    """
+    RAG model for generating answers to questions about political parties in the context of their manifestos and debates.
+
+    Args:
+    databases: list of VectorDatabase objects
+    llm: ChatOpenAI object, default is ChatOpenAI(model_name="gpt-3.5-turbo", max_tokens=2000, temperature=0)
+    k: int, number of documents to fetch from each database, default is 3
+    language: str, language of the generated answer, default is "Deutsch"
+    """
+
     def __init__(self, databases, llm=None, k=3, language="Deutsch"):
         self.databases = databases
         self.llm = llm
@@ -15,15 +25,34 @@ class RAG:
                 model_name="gpt-3.5-turbo", max_tokens=2000, temperature=0
             )
 
-    def get_documents_for_party(self, questions, party):
+    def get_documents_for_party(self, question, party):
+        """
+        Fetches documents from each database for a given party and a list of questions.
+
+        Args:
+        question: str, question
+        party: str, party name (one of "gruene", "spd", "cdu", "afd", "fdp", "linke")
+
+        Returns:
+        docs: dict, dictionary of documents for each source type ("manifestos" and "debates")
+        """
         docs = {}
         for db in self.databases:
             docs[db.source_type] = db.database.max_marginal_relevance_search(
-                questions, k=self.k, fetch_k=5, filter={"party": party}
+                question, k=self.k, fetch_k=5, filter={"party": party}
             )
         return docs
 
     def build_context_from_docs(self, docs):
+        """
+        Builds context string from documents for use in prompting.
+
+        Args:
+        docs: dict, dictionary of documents for each source type ("manifestos" and "debates")
+
+        Returns:
+        context: str, context string (formatted and human-readable excerpts from documents)
+        """
 
         context = ""
         for source_type in docs:
@@ -45,6 +74,16 @@ class RAG:
         return context
 
     def generate_prompt_for_party(self, question, party):
+        """
+        Generates a prompt for a given party and question.
+
+        Args:
+        question: str, question
+        party: str, party name (one of "gruene", "spd", "cdu", "afd", "fdp", "linke")
+
+        Returns:
+        prompt_dict: dict, dictionary containing the question, prompt, and documents for the party
+        """
         docs = self.get_documents_for_party(question, party)
         context = self.build_context_from_docs(docs)
         prompt = f"""   
@@ -62,31 +101,62 @@ class RAG:
             FRAGE DES NUTZERS: 
             {question}
             """
-        return {"question": question, "prompt": prompt, "docs": docs}
+        prompt_dict = {"question": question, "prompt": prompt, "docs": docs}
+        return prompt_dict
 
     def generate_prompts(self, question):
-        prompts = {
+        """
+        Generates prompts for each party given a question.
+
+        Args:
+        question: str, question
+
+        Returns:
+        prompts_dict: dict, dictionary containing the question, prompt, and documents for each party
+        """
+        prompts_dict = {
             party: self.generate_prompt_for_party(question, party)
             for party in self.parties
         }
-        return prompts
+        return prompts_dict
 
     def query(self, question):
-        response = self.generate_prompts(question)
+        """
+        Generates answers for each party given a question.
 
+        Args:
+        question: str, question
+
+        Returns:
+        response_dict: dict, dictionary containing the question, prompt, documents, and answer for each party
+        """
+        response_dict = self.generate_prompts(question)
+
+        # Run LLM on all prompts in parallel
         response_ = asyncio.run(
-            self.llm.abatch([response[party]["prompt"] for party in response.keys()])
+            self.llm.abatch(
+                [response_dict[party]["prompt"] for party in response_dict.keys()]
+            )
         )
 
         # Attach response content to party dictionary
-        for i, party in enumerate(response):
-            response[party]["answer"] = response_[i].content
+        for i, party in enumerate(response_dict):
+            response_dict[party]["answer"] = response_[i].content
 
-        response = self.format_response(response)
+        response_dict = self.format_response(response_dict)
 
-        return response
+        return response_dict
 
     def format_response(self, response):
+        """
+        Formats the response dictionary for simpler use in the app.
+
+        Args:
+        response: dict, dictionary containing the question, prompt, documents, and answer for each party
+
+        Returns:
+        response: dict, formatted dictionary containing the question, prompt, documents, and answer for each party
+        """
         q = response[list(response.keys())[0]]["question"]
         p = {party: response[party]["prompt"] for party in response.keys()}
         d = {
