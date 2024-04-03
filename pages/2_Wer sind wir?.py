86ea85e7a2dc0f161217db64b71f2323436a552a
josh-nowak <joshuanowak@icloud.com>
2024-04-03 10:32:25 +0200

    adjusted link formatting on teams page

--- pages/2_Wer sind wir?.py
+++ pages/2_Wer sind wir?.py
@@ -19,44 +19,26 @@ st.write(
 st.subheader("Christian Liedl")
 st.write(
     """
-    Christian hat einige Jahre als Physiker geforscht und sich seit 2024 auf Data Science und Künstliche Intelligenz spezialisiert.
+    Christian hat einige Jahre als Physiker geforscht und sich seit 2024 auf Data Science und Künstliche Intelligenz spezialisiert.  
+    [Github](https://github.com/cliedl) | [LinkedIn](https://www.linkedin.com/in/christian-liedl-2aaa03133/)
     """
 )
-
-col1, col2 = st.columns([0.15, 0.85])
-with col1:
-    st.link_button("Github", "https://github.com/cliedl")
-
-with col2:
-    st.link_button("Linkedin", "https://www.linkedin.com/in/christian-liedl-2aaa03133/")
 
 
 st.subheader("Anna Neifer")
 st.write(
     """
-Anna hat als Journalistin für die ARD, ZDF, funk und die Kooperarive Berlin gearbeitet. Seit 2023 lernt sie coden und ist jetzt als Data Scientist tätig.
+Anna hat als Journalistin für die ARD, ZDF, funk und die Kooperarive Berlin gearbeitet. Seit 2023 lernt sie coden und ist jetzt als Data Scientist tätig.  
+[Webseite](https://aneifer.de) | [LinkedIn](https://www.linkedin.com/in/anna-neifer/)
 
   """
 )
-
-col1, col2 = st.columns([0.15, 0.85])
-with col1:
-    st.link_button("Webseite", "https://aneifer.de")
-
-with col2:
-    st.link_button("Linkedin", "https://www.linkedin.com/in/anna-neifer/")
 
 st.subheader("Joshua Nowak")
 
 st.write(
     """
-Joshua hat als User Researcher und Psychologe verschiedene digitale Produkte mitgestaltet. Seit einigen Jahren programmiert er in Python und R und hat sich auf Data Science spezialisiert.
+Joshua hat als User Researcher und Psychologe verschiedene digitale Produkte mitgestaltet. Seit einigen Jahren programmiert er in Python und R und hat sich auf Data Science spezialisiert.  
+[Github](https://github.com/josh-nowak) | [LinkedIn](https://www.linkedin.com/in/nowakjoshua/)
   """
 )
-
-col1, col2 = st.columns([0.15, 0.85])
-with col1:
-    st.link_button("Github", "https://github.com/josh-nowak")
-
-with col2:
-    st.link_button("Linkedin", "https://www.linkedin.com/in/nowakjoshua/")
