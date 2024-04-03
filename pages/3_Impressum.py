f04b1264165b5711a0075b19aedc307efe41d943
cliedl <christian.liedl@gmx.net>
2024-04-03 12:07:10 +0200

    Added email to Impressum

--- pages/3_Impressum.py
+++ pages/3_Impressum.py
@@ -6,12 +6,9 @@ st.set_page_config(page_title="Electify - Impressum", page_icon="🇪🇺")
 st.header("Impressum", divider="blue")
 st.write(
     """
-    Electify GbR
-
-    Vertretungsberechtigte Gesellschafter: Christian Liedl, Anna Neifer, Joshua Nowak
-
-    Anschrift: .....
-
-    Email: ....
+    ###### Electify GbR  
+    Vertretungsberechtigte Gesellschafter: Christian Liedl, Anna Neifer, Joshua Nowak  
+    Anschrift: Folgt in Kürze  
+    Email: [electify.eu@gmail.com](mailto:electify.eu@gmail.com)
     """
 )
