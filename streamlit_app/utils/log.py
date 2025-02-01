ff4af5ccef401258b517847d70afab954008e5c5
cliedl <christian.liedl@gmx.net>
2025-02-01 14:10:57 +0100

    added util to log to mongodb

---
+++ streamlit_app/utils/log.py
@@ -0,0 +1,49 @@
+import os
+
+import dotenv
+from bson import ObjectId
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+dotenv.load_dotenv()
+
+def add_log_dict(dictionary: dict) -> str: 
+    """
+    Log a dictionary to the database and return the inserted id.
+    Args:
+        dictionary: The dictionary to log.
+    Returns:
+        The inserted id.
+    """
+    client = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))
+    db = client[os.getenv("DATABASE_NAME")]
+    collection = db[os.getenv("COLLECTION_NAME")]
+    result = collection.insert_one(dictionary)
+    return result.inserted_id
+
+
+def update_log_dict(_id: str, dictionary: dict):
+    """
+    Update a dictionary in the database.
+    Args:
+        _id: The id of the dictionary to update.
+        dictionary: The dictionary to update.
+    Returns:
+        The result of the update.
+    """
+
+    client = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))
+    db = client[os.getenv("DATABASE_NAME")]
+    collection = db[os.getenv("COLLECTION_NAME")]
+    result = collection.update_one(
+        filter={"_id": ObjectId(_id)}, 
+        update={"$set": dictionary}
+    )
+    return result
+
+if __name__ == "__main__":
+    pass
+    # _id = log_dict({"query": "What?", "answer": "test", "rating": "10"})
+    # print(_id)
+    # result = update_dict(_id, {"answer": "test2"})
+    # print(type(result))
