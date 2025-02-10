276f0b47c538b6b470b71e56ca598cf7e1c918cb
josh-nowak <joshuanowak@icloud.com>
2025-02-10 15:56:04 +0100

    fix: only show afd disclaimer if party name is shown

--- App.py
+++ App.py
@@ -399,7 +399,7 @@ if st.session_state.stage > 1:
             else:
                 st.header(f"{translate('party', st.session_state.language)} {p}")
 
-            if party == "afd":
+            if party == "afd" and show_party:
                 st.caption(f"⚠️ **{translate('warning-afd', st.session_state.language)}**")
 
             st.write(st.session_state.response["answer"][party])
